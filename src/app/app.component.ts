169eecaa575fa20ecca0396577474254c857ba54
Raghavendra m <raghavendram@Raghavendras-MacBook-Pro.local>
2019-02-21 12:28:26 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { GridOptions } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  gridOptions = {
+    columnDefs: this.columnDefs,
+    rowData: null,
+    enableFilter: true
+};
+  quickSearchValue: string = '';
+  columnDefs = [
+    {headerName: 'Make', field: 'make'},
+    {headerName: 'Model', field: 'model'},
+    {headerName: 'Price', field: 'price',editable: true}
+];
+
+rowData = [
+    {make: 'Toyota', model: 'Celica', price: 35000},
+    {make: 'Ford', model: 'Mondeo', price: 32000},
+    {make: 'Porsche', model: 'Boxter', price: 72000}
+];
+
+private onQuickFilterChanged() {
+  this.gridOptions.api.setQuickFilter(this.quickSearchValue);
 }
+ngOnInit(){
+  fetch('https://api.myjson.com/bins/15psn9')
+  .then(result => result.json())
+  .then(rowData => this.rowData = rowData);
+}
+
+}
+}
+
